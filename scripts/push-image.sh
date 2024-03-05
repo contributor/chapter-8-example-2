61369586c6cfcf1bae5a3880f7a7d5677ae332af
contributor <foss.contributor@gmail.com>
2024-03-05 19:57:42 +0200

    full deploy

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -19,5 +19,8 @@
 # : "$REGISTRY_UN"
 # : "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+# echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
+docker push $CONTAINER_REGISTRY/history:$VERSION
+docker push $CONTAINER_REGISTRY/recommendations:$VERSION
+
